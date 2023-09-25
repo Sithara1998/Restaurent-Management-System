830fcabf92c0eef754382367cee0b2db1beccd0e
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-09-25 20:29:50 +0530

    Add server-socket

--- server-app/src/main/java/lk/ijse/dep11/app/controllers/ServerSceneController.java
+++ server-app/src/main/java/lk/ijse/dep11/app/controllers/ServerSceneController.java
@@ -1,4 +1,46 @@
 package lk.ijse.dep11.app.controllers;
 
+import javafx.application.Platform;
+import shared.Order;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerSceneController {
+    public void initialize(){
+
+        new Thread(this::startServer).start();
+    }
+    private void startServer(){
+        try {
+            ServerSocket serverSocket = new ServerSocket(5050);
+            System.out.println("Waiting for a client order");
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Client order accepted");
+            new Thread(()->{
+                try {
+                    InputStream is = localSocket.getInputStream();
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    ObjectInputStream ois = new ObjectInputStream(bis);
+
+                    while(true){
+                        Order order = (Order) ois.readObject();
+                        Platform.runLater(() -> tblOrder.getItems().add(order));
+
+                    }
+                } catch (EOFException e) {
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- shared/src/main/java/shared/Items.java
+++ shared/src/main/java/shared/Items.java
@@ -1,3 +1,5 @@
+package shared;
+
 public enum Items {
     SUBMARINE("I-001"), FISH_BUN("I-002"), SAUSAGE_BUN("I-003"), PIZZA("I-004");
 
