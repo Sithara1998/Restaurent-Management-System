8f4d4a8a20717b3c95fa9fafe29870d0aa4b304d
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-09-25 18:53:44 +0530

    Create Project

---
+++ shared/src/main/java/Items.java
@@ -0,0 +1,9 @@
+public enum Items {
+    SUBMARINE("I-001"), FISH_BUN("I-002"), SAUSAGE_BUN("I-003"), PIZZA("I-004");
+
+    String id;
+
+    Items(String id) {
+        this.id = id;
+    }
+}
---
+++ shared/src/main/java/Order.java
@@ -0,0 +1,22 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order implements Serializable{
+    private String id;
+    private String name;
+    private String contact;
+    private ArrayList<Items> items;
+    private int quantity;
+
+
+
+}
+
