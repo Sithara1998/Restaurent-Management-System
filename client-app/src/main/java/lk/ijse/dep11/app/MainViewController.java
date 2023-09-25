1d81dbcf963e74a66e2ab3386add9d0924404071
Udari Rathnasiri <udarihashini@gmail.com>
2023-09-25 20:26:25 +0530

    Add Client UI

---
+++ client-app/src/main/java/lk/ijse/dep11/app/MainViewController.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep11.app;
+
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class MainViewController {
+    public AnchorPane root;
+    public TextField txtName;
+    public TextField txtContact;
+    public TableView tblOrder;
+    public ImageView imvPizza;
+    public ImageView ImvFish_Bun;
+    public ImageView ImvSubmarine;
+    public ImageView Imv_Sausage_Bun;
+}
